216808ca0987d60c319b15fcd1f0b15af2db0553
CarlosBlandon <practicante@eysingenieria.com>
2023-11-15 18:02:20 -0500

    creacion de la imagen en el dashboard

--- emtfront/src/App.js
+++ emtfront/src/App.js
@@ -1,24 +1,37 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Avatar } from 'antd';
 
 import Home from './views/Home';
 import Dashboard from './views/Dashboard';
 import Login from './components/Login';
 import Logout from './components/Logout';
 
+// Importar la imagen del usuario
+import userImage from './assets/images/6326055.png';
 
 const { Header, Sider, Content } = Layout;
 
 const App = () => {
+  const usuario = {
+    nombre: 'Nombre del Usuario',
+    // Usar la imagen importada
+    imagen: userImage,
+  };
+
   return (
     <Router>
       <Layout style={{ minHeight: '100vh' }}>
-        {/* Resto del código de la barra lateral y el encabezado */}
         <Sider width={80} theme="dark">
+          {/* Imagen del usuario en la parte superior */}
+          <div style={{ textAlign: 'center', padding: '16px' }}>
+            <Avatar size={48} src={usuario.imagen} />
+            <div style={{ marginTop: '8px', color: 'white' }}>{usuario.nombre}</div>
+          </div>
+          {/* Otras opciones de la barra lateral */}
           <Menu theme="dark" mode="vertical" defaultSelectedKeys={['1']}>
-            {/* Otras opciones de la barra lateral */}
+            {/* ... */}
           </Menu>
         </Sider>
         <Layout>
