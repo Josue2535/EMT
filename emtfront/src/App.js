1f45c7704932a5a2e48975f6143c65d486ff7317
CarlosBlandon <practicante@eysingenieria.com>
2023-11-15 17:58:12 -0500

    creacion del dashboard

--- emtfront/src/App.js
+++ emtfront/src/App.js
@@ -1,21 +1,41 @@
+// src/App.js
 import React from 'react';
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Layout, Menu } from 'antd';
 
+import Home from './views/Home';
+import Dashboard from './views/Dashboard';
 import Login from './components/Login';
 import Logout from './components/Logout';
 
-const App = ({ keycloak }) => {
+
+const { Header, Sider, Content } = Layout;
+
+const App = () => {
   return (
     <Router>
-      <div>
-        <h1>Aplicación React con Keycloak</h1>
-        <Routes>
-          <Route path="/login" element={<Login keycloak={keycloak} />} />
-          <Route path="/logout" element={<Logout keycloak={keycloak} />} />
-          {/* Other protected routes */}
-        </Routes>
-      </div>
+      <Layout style={{ minHeight: '100vh' }}>
+        {/* Resto del código de la barra lateral y el encabezado */}
+        <Sider width={80} theme="dark">
+          <Menu theme="dark" mode="vertical" defaultSelectedKeys={['1']}>
+            {/* Otras opciones de la barra lateral */}
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }}>
+            {/* Contenido del encabezado */}
+          </Header>
+          <Content style={{ margin: '16px' }}>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/logout" element={<Logout />} />
+            </Routes>
+          </Content>
+        </Layout>
+      </Layout>
     </Router>
   );
 };
--- emtfront/src/index.js
+++ emtfront/src/index.js
@@ -1,38 +1,12 @@
+// src/index.js o tu archivo de entrada principal
 import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-import ReactDOM from 'react-dom';
-import './index.css';
-import Keycloak from 'keycloak-js';
 
-// Muestra un mensaje de carga mientras se inicializa Keycloak
 ReactDOM.render(
-  <div>
-    <h1>Cargando...</h1>
-  </div>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
-
-const keycloak = Keycloak({
-  realm: 'TU_REALM',
-  url: 'URL_DEL_SERVIDOR_DE_KEYCLOAK',
-  clientId: 'TU_CLIENT_ID',
-});
-
-keycloak.init({ onLoad: 'login-required' }).then((authenticated) => {
-  if (authenticated) {
-    console.log('Usuario autenticado');
-    renderApp();
-  } else {
-    console.log('Autenticación fallida');
-  }
-});
-
-const renderApp = () => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App keycloak={keycloak} />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-};
