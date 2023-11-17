b183892599423553652e9d04a32f515a2552d85e
Josué Rodríguez <47890658+Josue2535@users.noreply.github.com>
2023-11-17 09:26:31 -0500

    Añado los dos formatos

---
+++ emtfront/src/views/FormatoInformacionPersonal.js
@@ -0,0 +1,14 @@
+// src/views/FormatoHistoriaClinica.js
+import React from 'react';
+
+const FormatoHistoriaClinica = () => {
+  // Lógica y contenido de la vista de Formato de Historia Clínica
+  return (
+    <div>
+      <h2>Formato de Informacion Personal</h2>
+      {/* Agregar contenido específico de Formato de Historia Clínica aquí */}
+    </div>
+  );
+};
+
+export default FormatoHistoriaClinica;
