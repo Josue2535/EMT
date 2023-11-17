3f1864477a5568a223d6b1d5534329819fd0e3dd
CarlosBlandon <practicante@eysingenieria.com>
2023-11-16 20:57:47 -0500

    crud usuario y paciente

--- emtfront/src/views/Paciente.js
+++ emtfront/src/views/Paciente.js
@@ -1,12 +1,118 @@
 // src/views/Paciente.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Table, Button, Modal, Form, Input, message } from 'antd';
+import { getPacientes, createPaciente, updatePaciente, deletePaciente } from '../api';
 
 const Paciente = () => {
-  // Lógica y contenido de la vista de Paciente
+  const [pacientes, setPacientes] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [form] = Form.useForm();
+  const [editingPaciente, setEditingPaciente] = useState(null);
+
+  const fetchPacientes = async () => {
+    try {
+      const data = await getPacientes();
+      setPacientes(data);
+    } catch (error) {
+      console.error('Error al obtener la lista de pacientes', error);
+      message.error('Error al obtener la lista de pacientes');
+    }
+  };
+
+  useEffect(() => {
+    fetchPacientes();
+  }, []);
+
+  const handleCreate = () => {
+    setEditingPaciente(null);
+    setModalVisible(true);
+  };
+
+  const handleEdit = (paciente) => {
+    setEditingPaciente(paciente);
+    form.setFieldsValue(paciente);
+    setModalVisible(true);
+  };
+
+  const handleDelete = async (pacienteId) => {
+    try {
+      await deletePaciente(pacienteId);
+      message.success('Paciente eliminado exitosamente');
+      fetchPacientes();
+    } catch (error) {
+      console.error('Error al eliminar el paciente', error);
+      message.error('Error al eliminar el paciente');
+    }
+  };
+
+  const handleModalOk = async () => {
+    try {
+      const values = await form.validateFields();
+      if (editingPaciente) {
+        await updatePaciente(editingPaciente.id, values);
+        message.success('Paciente actualizado exitosamente');
+      } else {
+        await createPaciente(values);
+        message.success('Paciente creado exitosamente');
+      }
+      setModalVisible(false);
+      fetchPacientes();
+    } catch (error) {
+      console.error('Error al guardar el paciente', error);
+      message.error('Error al guardar el paciente');
+    }
+  };
+
+  const handleModalCancel = () => {
+    setModalVisible(false);
+    form.resetFields();
+  };
+
+  const columns = [
+    { title: 'ID', dataIndex: 'id', key: 'id' },
+    { title: 'Nombre', dataIndex: 'nombre', key: 'nombre' },
+    { title: 'Apellido', dataIndex: 'apellido', key: 'apellido' },
+    {
+      title: 'Acciones',
+      dataIndex: 'acciones',
+      key: 'acciones',
+      render: (_, record) => (
+        <>
+          <Button type="primary" size="small" onClick={() => handleEdit(record)}>
+            Editar
+          </Button>
+          <Button type="danger" size="small" onClick={() => handleDelete(record.id)} style={{ marginLeft: '8px' }}>
+            Eliminar
+          </Button>
+        </>
+      ),
+    },
+  ];
+
   return (
     <div>
-      <h2>Paciente</h2>
-      {/* Agregar contenido específico de Paciente aquí */}
+      <h2>Pacientes</h2>
+      <Button type="primary" onClick={handleCreate} style={{ marginBottom: '16px' }}>
+        Crear Paciente
+      </Button>
+      <Table dataSource={pacientes} columns={columns} rowKey="id" />
+
+      <Modal
+        title={editingPaciente ? 'Editar Paciente' : 'Crear Paciente'}
+        visible={modalVisible}
+        onOk={handleModalOk}
+        onCancel={handleModalCancel}
+      >
+        <Form form={form} layout="vertical" name="paciente_form">
+          <Form.Item name="nombre" label="Nombre" rules={[{ required: true, message: 'Por favor ingrese el nombre' }]}>
+            <Input />
+          </Form.Item>
+          <Form.Item name="apellido" label="Apellido" rules={[{ required: true, message: 'Por favor ingrese el apellido' }]}>
+            <Input />
+          </Form.Item>
+          {/* Agrega más campos según tus necesidades */}
+        </Form>
+      </Modal>
     </div>
   );
 };
--- emtfront/src/views/Usuario.js
+++ emtfront/src/views/Usuario.js
@@ -1,12 +1,116 @@
-// src/views/Usuario.js
-import React from 'react';
+// src/views/UsuarioCrud.js
+import React, { useState, useEffect } from 'react';
+import { Table, Button, Modal, Form, Input, message } from 'antd';
+import { getUsers, createUser, updateUser, deleteUser } from '../api'; // Ajusta las funciones de la API según sea necesario
 
 const Usuario = () => {
-  // Lógica y contenido de la vista de Usuario
+  const [usuarios, setUsuarios] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [form] = Form.useForm();
+  const [editingUser, setEditingUser] = useState(null);
+
+  const fetchUsuarios = async () => {
+    try {
+      const data = await getUsers();
+      setUsuarios(data);
+    } catch (error) {
+      console.error('Error al obtener la lista de usuarios', error);
+      message.error('Error al obtener la lista de usuarios');
+    }
+  };
+
+  useEffect(() => {
+    fetchUsuarios();
+  }, []);
+
+  const handleCreate = () => {
+    setEditingUser(null);
+    setModalVisible(true);
+  };
+
+  const handleEdit = (user) => {
+    setEditingUser(user);
+    form.setFieldsValue(user);
+    setModalVisible(true);
+  };
+
+  const handleDelete = async (userId) => {
+    try {
+      await deleteUser(userId);
+      message.success('Usuario eliminado exitosamente');
+      fetchUsuarios();
+    } catch (error) {
+      console.error('Error al eliminar el usuario', error);
+      message.error('Error al eliminar el usuario');
+    }
+  };
+
+  const handleModalOk = async () => {
+    try {
+      const values = await form.validateFields();
+      if (editingUser) {
+        await updateUser(editingUser.id, values);
+        message.success('Usuario actualizado exitosamente');
+      } else {
+        await createUser(values);
+        message.success('Usuario creado exitosamente');
+      }
+      setModalVisible(false);
+      fetchUsuarios();
+    } catch (error) {
+      console.error('Error al guardar el usuario', error);
+      message.error('Error al guardar el usuario');
+    }
+  };
+
+  const handleModalCancel = () => {
+    setModalVisible(false);
+    form.resetFields();
+  };
+
+  const columns = [
+    { title: 'ID', dataIndex: 'id', key: 'id' },
+    { title: 'Nombre de Usuario', dataIndex: 'username', key: 'username' },
+    {
+      title: 'Acciones',
+      dataIndex: 'acciones',
+      key: 'acciones',
+      render: (_, record) => (
+        <>
+          <Button type="primary" size="small" onClick={() => handleEdit(record)}>
+            Editar
+          </Button>
+          <Button type="danger" size="small" onClick={() => handleDelete(record.id)} style={{ marginLeft: '8px' }}>
+            Eliminar
+          </Button>
+        </>
+      ),
+    },
+  ];
+
   return (
     <div>
-      <h2>Usuario</h2>
-      {/* Agregar contenido específico de Usuario aquí */}
+      <h2>Usuarios</h2>
+      <Button type="primary" onClick={handleCreate} style={{ marginBottom: '16px' }}>
+        Crear Usuario
+      </Button>
+      <Table dataSource={usuarios} columns={columns} rowKey="id" />
+
+      <Modal
+        title={editingUser ? 'Editar Usuario' : 'Crear Usuario'}
+        visible={modalVisible}
+        onOk={handleModalOk}
+        onCancel={handleModalCancel}
+      >
+        <Form form={form} layout="vertical" name="usuario_form">
+          <Form.Item name="username" label="Nombre de Usuario" rules={[{ required: true, message: 'Por favor ingrese el nombre de usuario' }]}>
+            <Input />
+          </Form.Item>
+          <Form.Item name="password" label="Contraseña" rules={[{ required: !editingUser, message: 'Por favor ingrese la contraseña' }]}>
+            <Input.Password />
+          </Form.Item>
+        </Form>
+      </Modal>
     </div>
   );
 };
