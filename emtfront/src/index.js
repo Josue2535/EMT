bba51e391491411a74482f5d5a14728cd066bf1e
Josué Rodríguez <47890658+Josue2535@users.noreply.github.com>
2023-11-20 14:26:05 -0500

    try to make login with keycloack

--- emtfront/src/index.js
+++ emtfront/src/index.js
@@ -1,12 +1,14 @@
-// src/index.js o tu archivo de entrada principal
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ReactKeycloakProvider } from '@react-keycloak/web';
+import keycloak from './Keycloak.js'; // Asegúrate de tener la configuración de Keycloak aquí
 import App from './App';
-
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ReactKeycloakProvider authClient={keycloak}>
+      <App />
+    </ReactKeycloakProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- emtfront/src/views/Home.js
+++ emtfront/src/views/Home.js
@@ -1,13 +1,36 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { useKeycloak } from '@react-keycloak/web';
+import { Button } from 'antd'; // Asegúrate de importar el componente de botón de antd
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const { keycloak, initialized } = useKeycloak();
+  const navigate = useNavigate();
 
- return (
-   <div>
-     <h1 className="text-green-800 text-4xl">Welcome to the Homepage</h1>
-   </div>
- );
+  // Verificar si Keycloak está inicializado
+  useEffect(() => {
+    if (initialized && keycloak.authenticated) {
+      console.log('User is authenticated');
+      // Aquí puedes realizar acciones específicas para usuarios autenticados
+    } else {
+      console.log('User is not authenticated');
+    }
+  }, [initialized, keycloak.authenticated]);
+
+  const handleLoginClick = () => {
+    keycloak.login(); // Redirige al inicio de sesión de Keycloak
+  };
+
+  return (
+    <div>
+      <h1 className="text-green-800 text-4xl">Welcome to the Homepage</h1>
+      {!keycloak.authenticated && (
+        <Button type="primary" onClick={handleLoginClick}>
+          Login
+        </Button>
+      )}
+    </div>
+  );
 };
 
 export default Home;
