30b191cacb60db387752fecc88c14e84b0ba3d34
CarlosBlandon <practicante@eysingenieria.com>
2023-11-21 13:32:05 -0500

    login pirobo

--- emtfront/src/App.js
+++ emtfront/src/App.js
@@ -1,6 +1,5 @@
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
 import { Layout, Menu, Avatar } from 'antd';
 import { FileTextOutlined, UserOutlined, BookOutlined, UserAddOutlined, SolutionOutlined, UserSwitchOutlined } from '@ant-design/icons';
 
@@ -109,10 +108,25 @@ const App = () => {
     isEnabled: true
   };
 
+  const [authenticated, setAuthenticated] = useState(false);
+
+  const handleLogin = () => {
+    // Lógica de autenticación aquí
+    // Setea el estado para indicar que el usuario está autenticado
+    setAuthenticated(true);
+  };
+
+  const handleLogout = () => {
+    // Lógica de cierre de sesión aquí
+    // Setea el estado para indicar que el usuario no está autenticado
+    setAuthenticated(false);
+  };
+
   return (
     <Router>
-      <Layout style={{ minHeight: '100vh' }}>
-        <Sider width={200} theme="dark">
+      {authenticated && (
+        <Layout style={{ minHeight: '100vh' }}>
+          <Sider width={200} theme="dark">
           <div style={{ textAlign: 'center', padding: '16px' }}>
             <Avatar size={48} src={usuario.imagen} />
             <div style={{ marginTop: '8px', color: 'white' }}>{usuario.nombre}</div>
@@ -183,7 +197,7 @@ const App = () => {
             </Menu.SubMenu>
             <Menu.SubMenu key="6.3" icon={<SolutionOutlined />} title="Formato de Informacion Personal">
               <Link to="/formatoInformacionPersonal">Formato de Información Personal</Link>
-              </Menu.SubMenu>
+            </Menu.SubMenu>
             <Menu.SubMenu key="8" icon={<UserAddOutlined />} title="Formato de Paciente">
               <Link to="/FormatoPaciente"></Link>
             </Menu.SubMenu>
@@ -200,28 +214,37 @@ const App = () => {
               </Menu.Item>
             </Menu.SubMenu>
           </Menu>
-        </Sider>
-        <Layout>
-          <Header style={{ background: '#fff', padding: 0 }}></Header>
-          <Content style={{ margin: '16px' }}>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/logout" element={<Logout />} />
-              <Route path="/historiaclinica" element={<HistoriaClinica />} />
-              <Route path="/rol" element={<Rol />} />
-              <Route path="/formatohistoriaclinica" element={<FormatoHistoriaClinica />} />
-              <Route path="/formatoInformacionPersonal" element={<FormatoInformacionPersonal />} />
-              <Route path="/formatoPaciente" element={<FormatoPaciente />} />
-              <Route path="/paciente" element={<Paciente />} />
-              <Route path="/informacionpersonal" element={<InformacionPersonal />} />
-              <Route path="/usuario" element={<Usuario />} />
-            </Routes>
-          </Content>
+          </Sider>
+          <Layout>
+            <Header style={{ background: '#fff', padding: 0 }}>
+              <div style={{ textAlign: 'right', marginRight: '16px' }}>
+                <Avatar size={48} src={usuario.imagen} />
+                <span style={{ marginLeft: '8px' }}>{usuario.nombre}</span>
+              </div>
+            </Header>
+            <Content style={{ margin: '16px' }}>
+              <Routes>
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/logout" element={<Logout onLogout={handleLogout} />} />
+                <Route path="/historiaclinica" element={<HistoriaClinica />} />
+                <Route path="/rol" element={<Rol />} />
+                <Route path="/formatohistoriaclinica" element={<FormatoHistoriaClinica />} />
+                <Route path="/formatoInformacionPersonal" element={<FormatoInformacionPersonal />} />
+                <Route path="/formatoPaciente" element={<FormatoPaciente />} />
+                <Route path="/paciente" element={<Paciente />} />
+                <Route path="/informacionpersonal" element={<InformacionPersonal />} />
+                <Route path="/usuario" element={<Usuario />} />
+              </Routes>
+            </Content>
+          </Layout>
         </Layout>
-      </Layout>
+      )}
+      {!authenticated && (
+        <Routes>
+          <Route path="/login" element={<Login onLogin={handleLogin} />} />
+        </Routes>
+      )}
     </Router>
   );
 };
--- emtfront/src/components/Login.js
+++ emtfront/src/components/Login.js
@@ -1,16 +1,70 @@
 import React from 'react';
 import { Button } from 'antd';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import keycloak from '../Keycloak'; 
+import loginImage from '../assets/images/6326055.png';
 
-const Login = ({ keycloak }) => {
-  const handleLogin = () => {
-    keycloak.login();
+const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+const LoginCard = styled(motion.div)`
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  padding: 16px;
+  max-width: 300px;
+  width: 100%;
+  text-align: center;
+`;
+
+const Title = styled.h2`
+  margin-bottom: 16px;
+  color: #333;
+`;
+
+const Image = styled.img`
+  border-radius: 50%;
+  margin-bottom: 16px;
+`;
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      await keycloak.init(); // Inicializa Keycloak
+      await keycloak.login(); // Inicia sesión con Keycloak
+
+      // Si la autenticación es exitosa, redirige al dashboard
+      if (keycloak.authenticated) {
+        navigate('/dashboard');
+      }
+    } catch (error) {
+      // Maneja errores de inicio de sesión
+      console.error('Error al iniciar sesión con Keycloak:', error);
+    }
   };
-
   return (
-    <div>
-      <h2>Login</h2>
-      <Button onClick={handleLogin}>Iniciar sesión con Keycloak</Button>
-    </div>
+    <LoginContainer>
+      <LoginCard
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <Image src={loginImage} alt="Login" width={120} height={120} />
+        <Title>Login</Title>
+        <Button type="primary" onClick={handleLogin}>
+          Iniciar sesión con Keycloak
+        </Button>
+      </LoginCard>
+    </LoginContainer>
   );
 };
 
