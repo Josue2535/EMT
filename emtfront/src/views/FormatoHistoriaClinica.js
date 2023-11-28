f676d548d08b1667ba860c2f954184971d75fc46
Josué Rodríguez <47890658+Josue2535@users.noreply.github.com>
2023-11-28 16:28:39 -0500

    Crea el formato de la historia clinica

--- emtfront/src/views/FormatoHistoriaClinica.js
+++ emtfront/src/views/FormatoHistoriaClinica.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { Modal, Button, Input, Checkbox, Table, Space, Form } from 'antd';
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import { useKeycloak } from '@react-keycloak/web';
 
 const FormatoHistoriaClinica = () => {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [formatos, setFormatos] = useState([]);
+  const { keycloak } = useKeycloak();
+
+  const [form] = Form.useForm();
+
   const [formatoData, setFormatoData] = useState({
     id: '',
     creationDate: '',
@@ -20,16 +25,20 @@ const FormatoHistoriaClinica = () => {
     fieldOptions: [''],
   });
 
-  const [form] = Form.useForm();
-
-  // useEffect para obtener la información de la API al cargar el componente
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://localhost:7208/ClinicalHistoryFormat');
+        const response = await fetch('https://localhost:7208/ClinicalHistoryFormat', {
+          headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${keycloak.token}`,
+          },
+        });
+
         if (!response.ok) {
           throw new Error(`Error al obtener datos: ${response.statusText}`);
         }
+
         const data = await response.json();
         setFormatos(data);
       } catch (error) {
@@ -38,39 +47,96 @@ const FormatoHistoriaClinica = () => {
     };
 
     fetchData();
-  }, []); // El array vacío asegura que se ejecute solo una vez al montar el componente
+  }, [keycloak.token]);
 
-  const showModal = () => {
+  const showModal = (formato = null) => {
+    if (formato) {
+      const validFields = Array.isArray(formato.validFields) ? [...formato.validFields] : [];
+      setFormatoData({
+        id: formato.id,
+        creationDate: formato.creationDate,
+        name: formato.name,
+        description: formato.description,
+        validFields: validFields,
+      });
+      form.setFieldsValue({
+        name: formato.name,
+        description: formato.description,
+        validFields: validFields,
+      });
+    } else {
+      // Si es una creación, restablecer el estado formatoData a un objeto vacío
+      setFormatoData({
+        id: '',
+        creationDate: '',
+        name: '',  // Asegurémonos de que estos campos estén inicializados correctamente
+        description: '',  // Asegurémonos de que estos campos estén inicializados correctamente
+        validFields: [],
+      });
+      form.setFieldsValue({
+        name: '',
+        description: '',
+        validFields: '',
+      }); // Solo restablecemos los campos, sin afectar los valores iniciales
+    }
+
     setIsModalVisible(true);
   };
 
-  const handleOk = async () => {
+  const handleOk = async (values) => {
     try {
       const url = 'https://localhost:7208/ClinicalHistoryFormat';
       const method = formatoData.id ? 'PUT' : 'POST';
-
+  
+      const requestBody = {
+        validFields: formatoData.validFields,
+        name: values.name,
+        description: values.description,
+      };
+  
       const response = await fetch(url, {
         method,
         headers: {
           'Content-Type': 'application/json',
+          Authorization: `Bearer ${keycloak.token}`,
         },
-        body: JSON.stringify({
-          validFields: formatoData.validFields,
-          name: formatoData.name,
-          Description: formatoData.description,
-        }),
+        body: JSON.stringify(requestBody),
       });
-
+  
       if (!response.ok) {
         throw new Error(
           `Error al ${formatoData.id ? 'editar' : 'crear'} el formato: ${response.statusText}`
         );
       }
-
+  
       const updatedFormatos = await response.json();
       setFormatos(updatedFormatos);
-
+  
       form.resetFields();
+  
+      if (!formatoData.id) {
+        setFormatoData({
+          id: '',
+          creationDate: '',
+          name: '',
+          description: '',
+          validFields: [],
+        });
+      }
+  
+      setIsModalVisible(false);
+    } catch (error) {
+      console.error(`Error al ${formatoData.id ? 'editar' : 'crear'} el formato:`, error);
+    }
+  };
+  
+  
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+    form.resetFields();
+
+    if (!formatoData.id) {
       setFormatoData({
         id: '',
         creationDate: '',
@@ -78,18 +144,7 @@ const FormatoHistoriaClinica = () => {
         description: '',
         validFields: [],
       });
-      setIsModalVisible(false);
-    } catch (error) {
-      console.error(
-        `Error al ${formatoData.id ? 'editar' : 'crear'} el formato:`,
-        error
-      );
     }
-  };
-
-  const handleCancel = () => {
-    setIsModalVisible(false);
-    form.resetFields();
   };
 
   const handleAddField = () => {
@@ -106,10 +161,10 @@ const FormatoHistoriaClinica = () => {
     });
   };
 
-  const handleChangeField = (e, index) => {
-    const { name, value } = e.target;
+  const handleChangeField = (e, index, field) => {
+    const { value } = e.target;
     const updatedFields = [...formatoData.validFields];
-    updatedFields[index][name] = value;
+    updatedFields[index][field] = value;
     setFormatoData((prevData) => ({
       ...prevData,
       validFields: updatedFields,
@@ -154,9 +209,14 @@ const FormatoHistoriaClinica = () => {
 
   const handleEdit = (index) => {
     const editedFormato = formatos[index];
-    setFormatoData(editedFormato);
-    setFormatos(formatos.filter((formato, i) => i !== index));
-    setIsModalVisible(true);
+    setFormatoData({
+      id: editedFormato.id,
+      creationDate: editedFormato.creationDate,
+      name: editedFormato.name,
+      description: editedFormato.description,
+      validFields: [...editedFormato.validFields],
+    });
+    showModal(editedFormato);
   };
 
   const handleDelete = (index) => {
@@ -189,14 +249,12 @@ const FormatoHistoriaClinica = () => {
   return (
     <div>
       <h2>Formato de Historia Clínica</h2>
-      <Button type="primary" onClick={showModal}>
+      <Button type="primary" onClick={() => showModal()}>
         Crear
       </Button>
 
-      {/* Tabla para mostrar la lista de formatos */}
       <Table dataSource={formatos} columns={columns} rowKey={(record, index) => index} />
 
-      {/* Modal para mostrar la información antes de crear/editar */}
       <Modal
         title="Crear/Editar Formato"
         visible={isModalVisible}
@@ -204,15 +262,26 @@ const FormatoHistoriaClinica = () => {
         onCancel={handleCancel}
         footer={null}
       >
-        <Form form={form} onFinish={handleOk} initialValues={formatoData}>
-          <Form.Item label="Nombre del Formato" name="name" rules={[{ required: true, message: 'Ingresa el nombre del formato' }]}>
+        <Form
+          form={form}
+          onFinish={handleOk}
+          initialValues={formatoData.id ? formatoData : undefined}
+        >
+          <Form.Item
+            label="Nombre del Formato"
+            name="name"
+            rules={[{ required: true, message: 'Ingresa el nombre del formato' }]}
+          >
             <Input />
           </Form.Item>
-          <Form.Item label="Descripción" name="description" rules={[{ required: true, message: 'Ingresa la descripción del formato' }]}>
+          <Form.Item
+            label="Descripción"
+            name="description"
+            rules={[{ required: true, message: 'Ingresa la descripción del formato' }]}
+          >
             <Input.TextArea />
           </Form.Item>
 
-          {/* Campos Válidos */}
           <h3>Añadir Campos</h3>
           {formatoData.validFields.map((field, index) => (
             <div key={index}>
@@ -244,7 +313,9 @@ const FormatoHistoriaClinica = () => {
                       value={option}
                       onChange={(e) => handleChangeOption(index, optionIndex, e.target.value)}
                     />
-                    <Button onClick={() => handleDeleteOption(index, optionIndex)}>Eliminar Opción</Button>
+                    <Button onClick={() => handleDeleteOption(index, optionIndex)}>
+                      Eliminar Opción
+                    </Button>
                   </div>
                 ))}
                 <Button onClick={() => handleAddOption(index)}>Añadir Opción</Button>
