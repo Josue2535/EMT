81df8bcea40ed542c2104355cb502382c569bd37
Josué Rodríguez <47890658+Josue2535@users.noreply.github.com>
2023-12-14 16:50:51 -0500

    create hc

--- emtfront/src/views/VerHistoriaClinica.js
+++ emtfront/src/views/VerHistoriaClinica.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import { Select, Table, Button, Modal } from 'antd';
+import { Select, Table, Button, Modal, Input } from 'antd';
 import { useKeycloak } from '@react-keycloak/web';
 
 const VerHistoriaClinica = () => {
@@ -12,74 +12,74 @@ const VerHistoriaClinica = () => {
   const [selectOptions, setSelectOptions] = useState([]);
   const [selectedAttachment, setSelectedAttachment] = useState(null);
 
+  const fetchData = async () => {
+    try {
+      // Obtener datos de la historia clínica
+      const responseHistoriaClinica = await fetch(`https://localhost:7208/api/ClinicalHistory/user/${pacienteId}`, {
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${keycloak.token}`,
+        },
+      });
+
+      if (!responseHistoriaClinica.ok) {
+        throw new Error(`Error al obtener datos de la historia clínica: ${responseHistoriaClinica.statusText}`);
+      }
+
+      const dataHistoriaClinica = await responseHistoriaClinica.json();
+      console.log('Data recibida de la historia clínica:', dataHistoriaClinica);
+
+      // Mapear datos de la historia clínica
+      const formattedData = {
+        id: dataHistoriaClinica.id,
+        created: dataHistoriaClinica.created,
+        patientId: dataHistoriaClinica.patientId,
+        attachments: dataHistoriaClinica.attachments.map((attachment) => ({
+          id: attachment.id,
+          created: attachment.created,
+          fields: attachment.fields,
+          nameFormat: attachment.nameFormat,
+        })),
+      };
+
+      setHistoriaClinica(formattedData);
+
+      // Configurar opciones para el select (dropdown)
+      const options = formattedData.attachments.map((attachment) => ({
+        value: attachment.id,
+        label: attachment.nameFormat,
+      }));
+      setSelectOptions(options);
+    } catch (error) {
+      console.error('Error al obtener datos de la historia clínica:', error);
+    }
+  };
+
+  const fetchClinicalHistoryFormats = async () => {
+    try {
+      // Obtener datos de los formatos de historia clínica
+      const responseClinicalHistoryFormats = await fetch('https://localhost:7208/ClinicalHistoryFormat/GetByRole', {
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${keycloak.token}`,
+        },
+      });
+
+      if (!responseClinicalHistoryFormats.ok) {
+        throw new Error(`Error al obtener datos de los formatos de historia clínica: ${responseClinicalHistoryFormats.statusText}`);
+      }
+
+      const dataClinicalHistoryFormats = await responseClinicalHistoryFormats.json();
+      console.log('Data recibida de los formatos de historia clínica:', dataClinicalHistoryFormats);
+
+      // Mapear datos de los formatos de historia clínica
+      setClinicalHistoryFormats(dataClinicalHistoryFormats);
+    } catch (error) {
+      console.error('Error al obtener datos de los formatos de historia clínica:', error);
+    }
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        // Obtener datos de la historia clínica
-        const responseHistoriaClinica = await fetch(`https://localhost:7208/api/ClinicalHistory/user/${pacienteId}`, {
-          headers: {
-            Accept: 'application/json',
-            Authorization: `Bearer ${keycloak.token}`,
-          },
-        });
-
-        if (!responseHistoriaClinica.ok) {
-          throw new Error(`Error al obtener datos de la historia clínica: ${responseHistoriaClinica.statusText}`);
-        }
-
-        const dataHistoriaClinica = await responseHistoriaClinica.json();
-        console.log('Data recibida de la historia clínica:', dataHistoriaClinica);
-
-        // Mapear datos de la historia clínica
-        const formattedData = {
-          id: dataHistoriaClinica.id,
-          created: dataHistoriaClinica.created,
-          patientId: dataHistoriaClinica.patientId,
-          attachments: dataHistoriaClinica.attachments.map((attachment) => ({
-            id: attachment.id,
-            created: attachment.created,
-            fields: attachment.fields,
-            nameFormat: attachment.nameFormat,
-          })),
-        };
-
-        setHistoriaClinica(formattedData);
-
-        // Configurar opciones para el select (dropdown)
-        const options = formattedData.attachments.map((attachment) => ({
-          value: attachment.id,
-          label: attachment.nameFormat,
-        }));
-        setSelectOptions(options);
-      } catch (error) {
-        console.error('Error al obtener datos de la historia clínica:', error);
-      }
-    };
-
-    const fetchClinicalHistoryFormats = async () => {
-      try {
-        // Obtener datos de los formatos de historia clínica
-        const responseClinicalHistoryFormats = await fetch('https://localhost:7208/ClinicalHistoryFormat/GetByRole', {
-          headers: {
-            Accept: 'application/json',
-            Authorization: `Bearer ${keycloak.token}`,
-          },
-        });
-
-        if (!responseClinicalHistoryFormats.ok) {
-          throw new Error(`Error al obtener datos de los formatos de historia clínica: ${responseClinicalHistoryFormats.statusText}`);
-        }
-
-        const dataClinicalHistoryFormats = await responseClinicalHistoryFormats.json();
-        console.log('Data recibida de los formatos de historia clínica:', dataClinicalHistoryFormats);
-
-        // Mapear datos de los formatos de historia clínica
-        setClinicalHistoryFormats(dataClinicalHistoryFormats);
-      } catch (error) {
-        console.error('Error al obtener datos de los formatos de historia clínica:', error);
-      }
-    };
-
     fetchData();
     fetchClinicalHistoryFormats();
   }, [pacienteId, keycloak.token]);
@@ -106,8 +106,35 @@ const VerHistoriaClinica = () => {
     },
   ];
 
-  const handleCreate = (value) => {
+  const handleCreate = async (value) => {
     console.log('Crear nueva entrada con ID:', value);
+    await createClinicalHistory();
+  };
+
+  const createClinicalHistory = async () => {
+    try {
+      const response = await fetch('https://localhost:7208/api/ClinicalHistory', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          Authorization: `Bearer ${keycloak.token}`,
+        },
+        body: JSON.stringify({
+          patientId: pacienteId,
+          attachments: [],
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error al crear la historia clínica: ${response.statusText}`);
+      }
+
+      // Después de crear la historia clínica, volver a cargar la información
+      fetchData();
+    } catch (error) {
+      console.error('Error al crear la historia clínica:', error);
+    }
   };
 
   const handleViewAttachment = (attachment) => {
@@ -142,8 +169,6 @@ const VerHistoriaClinica = () => {
       <p style={{ fontSize: '18px' }}>ID del Paciente: {pacienteId}</p>
       <p style={{ fontSize: '18px' }}>Fecha de Creación: {historiaClinica.created}</p>
 
-      
-
       <Select style={{ width: 300 }} placeholder="Seleccione un formato">
         {clinicalHistoryFormats.map((format) => (
           <Select.Option key={format.id} value={format.id}>
@@ -151,6 +176,8 @@ const VerHistoriaClinica = () => {
           </Select.Option>
         ))}
       </Select>
+
+      <Button type="primary" onClick={() => handleCreate()}>Crear</Button>
 
       <Table dataSource={historiaClinica.attachments} columns={columns} rowKey="id" />
 
